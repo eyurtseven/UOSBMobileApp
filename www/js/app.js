09ba2809ccf672d739a48394dfa55b072571e317
emre <ben@emreyurtseven.com>
2015-08-02 15:48:26 +0300

    icon ve splash screen için resources eklendi

--- www/js/app.js
+++ www/js/app.js
@@ -8,6 +8,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
     .run(function ($ionicPlatform) {
         $ionicPlatform.ready(function () {
+
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
             // for form inputs)
             if (window.cordova && window.cordova.plugins.Keyboard) {
@@ -128,7 +129,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
         ;
         // if none of the above states are matched, use this as the fallback
-        $urlRouterProvider.otherwise('/app/companies');
+        $urlRouterProvider.otherwise('/app/units');
     })
 
     .service('CompanyService', function ($http) {
