3f066eca2afeb6ab24a7ea9fc2a404405ed4c919
emre <ben@emreyurtseven.com>
2015-08-17 23:32:54 +0300

    internet bağlantı kontrolü eklendi

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -67,134 +67,176 @@ angular.module('starter.controllers', [])
 
     })
 
-    .controller('NewsCtrl', function ($scope, NewsService, $ionicLoading) {
+    .controller('NewsCtrl', function ($scope, NewsService, $ionicLoading, $ionicPlatform, $state, $ionicPopup, $ionicHistory) {
+        $ionicHistory.currentView($ionicHistory.backView());
 
-        $ionicLoading.show({
-            template: 'Yükleniyor...'
-        });
+        $ionicPlatform.ready(function() {
 
-        $scope.news = [];
+            if(window.Connection) {
+                if (navigator.connection.type == Connection.NONE) {
+                    $ionicPopup.alert({
+                        title: "İnternet Bağlantı Hatası",
+                        content: "İnternet erişiminizi kontrol ediniz."
+                    }).then(function (result) {
+                        $state.go('app.contact');
+                        return;
+                    });
+                } else{
+                    $ionicLoading.show({
+                        template: 'Yükleniyor...'
+                    });
 
-        NewsService.getNews().then(function (response) {
+                    $scope.news = [];
 
-            angular.forEach(response.data, function (item, i) {
+                    NewsService.getNews().then(function (response) {
 
-                if (item.status == "publish") {
+                        angular.forEach(response.data, function (item, i) {
 
-                    var itemText = item.title;
-                    $scope.news.push({title: itemText, image: item.featured_image.source, id: item.ID});
-                    $ionicLoading.hide();
-                }
-            });
+                            if (item.status == "publish") {
 
-        });
+                                var itemText = item.title;
+                                $scope.news.push({title: itemText, image: item.featured_image.source, id: item.ID});
+                                $ionicLoading.hide();
+                            }
+                        });
 
-    })
-
-    .controller('NewsDetailCtrl', function ($scope, $stateParams, NewsService, $ionicLoading) {
-
-        $ionicLoading.show({
-            template: 'Yükleniyor...'
-        });
-
-
-        NewsService.getNews().then(function (response) {
-
-            angular.forEach(response.data, function (item, i) {
-
-                if (item.ID == $stateParams.newsId) {
-
-                    $scope.newsId = item.Id;
-                    $scope.newsImage = item.featured_image.source;
-                    $scope.content = item.content;
-                    $scope.newsTitle = item.title;
-                    $ionicLoading.hide();
-
-                }
-            });
-
-        });
-
-    })
-
-    .controller('GoToCompanyCtrl', function ($scope, $stateParams, CompanyService, GlobalFunctionService, $ionicLoading) {
-
-        ionic.Platform.ready(function () {
-
-
-            $ionicLoading.show({
-                template: 'Yükleniyor...'
-            });
-
-            var companyId = $stateParams.companyId;
-
-            CompanyService.getCompanyData().then(function (response) {
-
-                var companies = response.data;
-
-                var selectedCompany = GlobalFunctionService.findById(companies, companyId);
-
-                $scope.selectedCompany = selectedCompany;
-
-                $scope.myLocation = {
-                    lng: "",
-                    lat: ""
-                };
-
-                $scope.drawMap = function (position) {
-
-                    $scope.$apply(function () {
-
-                        $scope.myLocation.lng = position.coords.longitude;
-                        $scope.myLocation.lat = position.coords.latitude;
-
-                        initialize();
                     });
                 }
-                navigator.geolocation.getCurrentPosition($scope.drawMap, function () {
-                }, {enableHighAccuracy: true});
-                var directionsDisplay;
-                var directionsService = new google.maps.DirectionsService();
-                var map;
+            }
 
-                function initialize() {
-                    directionsDisplay = new google.maps.DirectionsRenderer();
-                    var inticor = new google.maps.LatLng($scope.myLocation.lat, $scope.myLocation.lng);
+        });
 
-                    var mapOptions =
-                    {
-                        zoom: 9,
-                        center: inticor,
-                        mapTypeId: google.maps.MapTypeId.ROADMAP,
-                    };
+    })
 
-                    map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+    .controller('NewsDetailCtrl', function ($scope, $stateParams, NewsService, $ionicLoading, $ionicHistory, $ionicPlatform, $ionicPopup) {
 
-                    directionsDisplay.setMap(map);
-                    calcRoute();
+        $ionicHistory.currentView($ionicHistory.backView());
 
+        $ionicPlatform.ready(function() {
+
+            if(window.Connection) {
+                if (navigator.connection.type == Connection.NONE) {
+                    $ionicPopup.alert({
+                        title: "İnternet Bağlantı Hatası",
+                        content: "İnternet erişiminizi kontrol ediniz."
+                    }).then(function (result) {
+                        $state.go('app.contact');
+                        return;
+                    });
+                } else{
+                    $ionicLoading.show({
+                        template: 'Yükleniyor...'
+                    });
+
+                    NewsService.getNews().then(function (response) {
+
+                        angular.forEach(response.data, function (item, i) {
+
+                            if (item.ID == $stateParams.newsId) {
+
+                                $scope.newsId = item.Id;
+                                $scope.newsImage = item.featured_image.source;
+                                $scope.content = item.content;
+                                $scope.newsTitle = item.title;
+                                $ionicLoading.hide();
+
+                            }
+                        });
+                    });
                 }
+            }
+        });
+    })
 
-                google.maps.event.addDomListener(window, 'load', initialize);
+    .controller('GoToCompanyCtrl', function ($scope, $stateParams, CompanyService, GlobalFunctionService, $ionicLoading, $ionicHistory, $ionicPlatform, $ionicPopup) {
 
-                function calcRoute() {
-                    var start = $scope.myLocation.lat + "," + $scope.myLocation.lng;
-                    var end = selectedCompany.Location.Latitude + "," + selectedCompany.Location.Longitude;
+        $ionicPlatform.ready(function() {
 
-                    var request = {
-                        origin: start,
-                        destination: end,
-                        travelMode: google.maps.TravelMode.DRIVING
-                    };
-                    directionsService.route(request, function (response, status) {
-                        if (status == google.maps.DirectionsStatus.OK) {
-                            directionsDisplay.setDirections(response);
+            if(window.Connection) {
+                if (navigator.connection.type == Connection.NONE) {
+                    $ionicPopup.alert({
+                        title: "İnternet Bağlantı Hatası",
+                        content: "İnternet erişiminizi kontrol ediniz."
+                    }).then(function (result) {
+                        $state.go('app.contact');
+                        return;
+                    });
+                } else{
+                    $ionicLoading.show({
+                        template: 'Yükleniyor...'
+                    });
+
+                    var companyId = $stateParams.companyId;
+
+                    CompanyService.getCompanyData().then(function (response) {
+
+                        var companies = response.data;
+
+                        var selectedCompany = GlobalFunctionService.findById(companies, companyId);
+
+                        $scope.selectedCompany = selectedCompany;
+
+                        $scope.myLocation = {
+                            lng: "",
+                            lat: ""
+                        };
+
+                        $scope.drawMap = function (position) {
+
+                            $scope.$apply(function () {
+
+                                $scope.myLocation.lng = position.coords.longitude;
+                                $scope.myLocation.lat = position.coords.latitude;
+
+                                initialize();
+                            });
                         }
-                    });
-                }
+                        navigator.geolocation.getCurrentPosition($scope.drawMap, function () {
+                        }, {enableHighAccuracy: true});
+                        var directionsDisplay;
+                        var directionsService = new google.maps.DirectionsService();
+                        var map;
 
-            });
-            $ionicLoading.hide();
+                        function initialize() {
+                            directionsDisplay = new google.maps.DirectionsRenderer();
+                            var inticor = new google.maps.LatLng($scope.myLocation.lat, $scope.myLocation.lng);
+
+                            var mapOptions =
+                            {
+                                zoom: 9,
+                                center: inticor,
+                                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                            };
+
+                            map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+
+                            directionsDisplay.setMap(map);
+                            calcRoute();
+
+                        }
+
+                        google.maps.event.addDomListener(window, 'load', initialize);
+
+                        function calcRoute() {
+                            var start = $scope.myLocation.lat + "," + $scope.myLocation.lng;
+                            var end = selectedCompany.Location.Latitude + "," + selectedCompany.Location.Longitude;
+
+                            var request = {
+                                origin: start,
+                                destination: end,
+                                travelMode: google.maps.TravelMode.DRIVING
+                            };
+                            directionsService.route(request, function (response, status) {
+                                if (status == google.maps.DirectionsStatus.OK) {
+                                    directionsDisplay.setDirections(response);
+                                }
+                            });
+                        }
+
+                    });
+                    $ionicLoading.hide();
+                }
+            }
         });
     })
     .controller('PlaylistCtrl', function ($scope, $stateParams) {
@@ -206,36 +248,51 @@ angular.module('starter.controllers', [])
     .controller('AboutCtrl', function ($scope, $stateParams) {
 
     })
-    .controller('ContactCtrl', function ($scope, $ionicLoading, $compile) {
+    .controller('ContactCtrl', function ($scope, $ionicLoading, $compile, $ionicHistory, $ionicPlatform, $ionicPopup) {
 
+        $ionicHistory.currentView($ionicHistory.backView());
 
-        function initialize() {
-            var myLatLng = new google.maps.LatLng(38.677179, 29.220291);
-            var mapCanvas = document.getElementById('map');
-            var mapOptions = {
-                center: myLatLng,
-                zoom: 15,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
+        $ionicPlatform.ready(function() {
+
+            if(window.Connection) {
+                if (navigator.connection.type == Connection.NONE) {
+                    $ionicPopup.alert({
+                        title: "İnternet Bağlantı Hatası",
+                        content: "İnternet erişiminizi kontrol ediniz."
+                    }).then(function (result) {
+                        $state.go('app.contact');
+                        return;
+                    });
+                } else{
+                    function initialize() {
+                        var myLatLng = new google.maps.LatLng(38.677179, 29.220291);
+                        var mapCanvas = document.getElementById('map');
+                        var mapOptions = {
+                            center: myLatLng,
+                            zoom: 15,
+                            mapTypeId: google.maps.MapTypeId.ROADMAP
+                        }
+                        var map = new google.maps.Map(mapCanvas, mapOptions)
+                        var contentString = "<div><a>Uşak Organize Sanayi Bölgesi</a></div>";
+                        var compiled = $compile(contentString)($scope);
+
+                        var infowindow = new google.maps.InfoWindow({
+                            content: compiled[0]
+                        });
+
+                        var marker = new google.maps.Marker({
+                            position: myLatLng,
+                            map: map,
+                            title: 'Uluru (Ayers Rock)'
+                        });
+
+                        $scope.map = map;
+                    }
+
+                    initialize();
+                }
             }
-            var map = new google.maps.Map(mapCanvas, mapOptions)
-            var contentString = "<div><a>Uşak Organize Sanayi Bölgesi</a></div>";
-            var compiled = $compile(contentString)($scope);
-
-            var infowindow = new google.maps.InfoWindow({
-                content: compiled[0]
-            });
-
-            var marker = new google.maps.Marker({
-                position: myLatLng,
-                map: map,
-                title: 'Uluru (Ayers Rock)'
-            });
-
-            $scope.map = map;
-        }
-
-        initialize();
-//        google.maps.event.addDomListener(window, 'load', initialize);
+        });
 
     })
     .controller('ChairmanCtrl', function ($scope, $stateParams) {
